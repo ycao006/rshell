eaff53d39eedf3880d2ebff0ea026ff9f9670ea6
Yujie Cao <ycao006@hammer.cs.ucr.edu>
2015-04-12 22:55:44 -0700

    rshell version1.0

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@
 #include <stdlib.h>//libray for exit
 #include <sys/types.h> 
 #include <sys/wait.h>// library for wait
+#include <array>
 using namespace std;
 
 
 int main(int argc, char** argv){
-
-	cout<<argc<<endl;
+	char* argv1[argc-1];
+	
+//	cout<<argc<<endl;
 	for (int i=0; i<argc; i++){
-		cout<< argv[i]<<" "<<endl;
+//		cout<< argv[i]<<" "<<endl;
+			argv1[i]=argv[i+1];
+			cout << argv1[i] << ' ';
 	}
 
+	cout<<endl;
+
+	for (int i=0; i<argc; i++){
 	int pid=fork();
 	if(pid == -1){
 		perror("There was a error in fork line 19"); //syscall to output error
@@ -21,10 +28,10 @@ int main(int argc, char** argv){
 	}
 
 	else if(pid == 0){//if it is the child process running
+
 		cout<<"This is the child process"<<endl;
-		cout<<"THis is the argv[2]: "<<argv[2]<<endl;
-		int result_execvp = execvp(argv[2], argv);
-		if(result_execvp==-1){
+		cout<<"arrgv1.size(): "<<argv1.size()<<endl;
+		if( execvp(argv1[0], argv1)==-1){
 			perror("There was an error in execvp line 27");
 			exit(1); //when this child process finishes, then kills this process
 		}
@@ -34,10 +41,10 @@ int main(int argc, char** argv){
 		int result_wait=wait(0);//wait wait for the child process
 		if(result_wait==-1){
 			perror("There was an error in wait line35");			
-		
 		}
 	}	
-	
+	}
+
 	return 0;
 }
 
