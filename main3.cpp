0397145140340f4d275b6646f47fc5847c1e7518
Yujie Cao <ycao006@hammer.cs.ucr.edu>
2015-04-18 00:23:34 -0700

    main3.cpp works for basic operations

--- main3.cpp
+++ main3.cpp
@@ -23,7 +23,12 @@ int main(){
 	char  userInput[maxCap]; 
 	string input;
 	int status;
-	bool pass;
+	bool pass=true;
+	bool connectorShow=false;
+	char* temp[maxCap];
+	int pos1=0;
+	int pos2=0;
+
 	getline(cin,input);
 
 	for (I = 0; I < maxCap; ++I) {
@@ -32,24 +37,101 @@ int main(){
 	//convert string into cstring;
 	//breaks the cstring into parts and store them into input array commandList;
 	inputReform(input, commandList);
-	cout << "inputReform is fine" << endl;
+//	cout << "inputReform is fine" << endl;
 	//display the input array
-	cout << "commandList includes: " << endl;
-	int i=0;
-	while(commandList[i]!=NULL){
+//	cout << "commandList includes: " << endl;
+	int i = 0;
+	while(commandList[i] != NULL){
 		cout << commandList[i] << endl;
 		i++;	
 	}		
-	cout << "old code works perfect "<<endl;
-	
-	syscall(commandList,pass,status);
-	
-	if(pass){
-		cout << " the execvp function works "<<endl;
+	cout << " old code works perfect "<<endl;
+	//run the input array
+	i=0;
+	while(commandList[i] != NULL){
+		cout << "test pos: " << i << "   pos1: " <<pos1 << " pos2: " << pos2 <<endl;
+		if(strcmp(commandList[i],"||") == 0){
+			connectorShow=true;
+			int j = 0;
+			while(j < pos2-pos1){
+				temp[j] = commandList[j+pos1];		
+				j++;
+			}
+			syscall(temp,pass,status);
+			if(pass){
+				cout <<endl << " the execvp function works "<<endl;
+			}
+			if(!pass){
+				cout << " the execvp fucntion does not work"<<endl;		
+			}
+			if(pass){
+				break;
+			}
+			else{
+				pos1 = pos2+1;
+			}
+		}
+
+		else if(strcmp(commandList[i],"&&") == 0){
+			connectorShow=true;
+			int j = 0;
+			while(j < pos2-pos1){
+				temp[j] = commandList[j+pos1];		
+				j++;
+			}
+			syscall(temp,pass,status);
+			if(pass){
+				cout <<endl << " the execvp function works "<<endl;
+			}
+			if(!pass){
+				cout << " the execvp fucntion does not work"<<endl;		
+			}
+			if(!pass){
+				break;
+			}
+			else{
+				pos1 = pos2+1;
+			}
+
+		}
+
+		else if(strcmp(commandList[i],";") == 0){
+			connectorShow=true;
+			int j = 0;
+			while(j < pos2-pos1){
+				temp[j] = commandList[j+pos1];		
+				j++;
+			}
+			syscall(temp,pass,status);
+			if(pass){
+				cout <<endl << " the execvp function works "<<endl;
+			}
+			if(!pass){
+				cout << " the execvp fucntion does not work"<<endl;		
+			}
+			pos1=pos2+1;
+		}	
+		i++;
+		pos2=i;
+		
+		if(commandList[i]==NULL){
+			int j = 0;
+			while(j < pos2-pos1){
+				temp[j] = commandList[j+pos1];		
+				j++;
+			}
+			syscall(temp,pass,status);
+			if(pass){
+				cout <<endl << " the execvp function works "<<endl;
+			}
+			if(!pass){
+				cout << " the execvp fucntion does not work"<<endl;		
+			}
+		}	
+		
+					
 	}
-	if(!pass){
-		cout << " the execvp fucntion does not work"<<endl;		
-	}
+
 	return 0;
 
 }
@@ -63,24 +145,24 @@ int main(){
 		// ignore the string after #
 		if(pos != string::npos){
 			str = str.substr(0,pos);
-			cout << "str: "<< str << " str size:  " << str.length() << endl << endl << endl;
+//			cout << "str: "<< str << " str size:  " << str.length() << endl << endl << endl;
 		}
 	
 		connector_format(str);
-		cout << "connector function is fine " << endl;
+//		cout << "connector function is fine " << endl;
 		test=str.c_str();//test
-		cout << "c_str function is fine " << endl;
+//		cout << "c_str function is fine " << endl;
 		strcpy(cstr , str.c_str());
-		cout << "strcpy function is fine" << endl;
+//		cout << "strcpy function is fine" << endl;
 		array[i] = strtok(cstr , " ");
 		while(array[i] != NULL){
 			i++;
 			array[i] = strtok(NULL," ");
 		}
 		i=0;
-		cout << "stroke function is fine"<<endl;
+//		cout << "stroke function is fine"<<endl;
 		while(array[i] != NULL){
-			cout<<" i: "<<i<<" array[i] "<<array[i]<<endl;		
+//			cout<<" i: "<<i<<" array[i] "<<array[i]<<endl;		
 			i++;
 		}
 		return;
@@ -91,15 +173,15 @@ int main(){
  		size_t found = 0;
 		int strl = 0;
 		string connectors[3] = {"||", "&&", ";"};
-		string Newconnectors[3] = {" || ", " && ", "   "}; 
+		string Newconnectors[3] = {" || ", " && ", " ; "}; 
 	
 		for (unsigned int i=0; i<3; i++){
 			found = 0;
-			cout << connectors[i] << endl;     
+//			cout << connectors[i] << endl;     
 			while(found != string::npos){
-				cout << "found0 " << found << endl;
+//				cout << "found0 " << found << endl;
 				found = str.find(connectors[i],found);
-				cout << "found1 " << found << endl;
+//				cout << "found1 " << found << endl;
 				
 				if(found!=string::npos){
 					str.replace(found , connectors[i].length() , Newconnectors[i]);
@@ -112,8 +194,8 @@ int main(){
 					}
 				} 
 				
-				cout << "found2 " << found << endl;
-				cout << "new str " << str << " new str size: " << str.length() << endl;
+//				cout << "found2 " << found << endl;
+//				cout << "new str " << str << " new str size: " << str.length() << endl;
 			}
 		}
 
@@ -122,7 +204,7 @@ int main(){
 
 	void syscall(char* argv[], bool& pass, int& status){
 	//	int status;	
-		cout << " argv: " << argv << endl;		
+//		cout << " argv: " << argv << endl;		
 		int pid = fork();	
 		if(pid == -1){
 			perror("There was a error in fork line 19"); //syscall to output error
@@ -149,7 +231,7 @@ int main(){
 		}
 
 		if(WEXITSTATUS(status)==EXIT_SUCCESS){
-			cout << "child process success and exit" << endl;
+			cout <<endl <<"child process success and exit" << endl;
 			pass=true;
 		}
 		else if(WEXITSTATUS(status)==EXIT_FAILURE){
